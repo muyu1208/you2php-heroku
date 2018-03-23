e99e41179689641ecb6df878aea262b33cf79ede
muyu1208 <37690965+muyu1208@users.noreply.github.com>
2018-03-23 17:02:30 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
+define('APIKEY', AIzaSyAMZvj3riKqYvCLG7v0x8NQpG20GxcnBJQ("APIKEY"));
 define('GJ_CODE', getenv("GJ_CODE"));
 define('SITE_NAME',getenv("SITE_NAME"));
 define('TITLENAME', getenv("TITLENAME"));
